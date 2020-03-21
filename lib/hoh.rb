4ab2443781d52b3b09bb2aa75c0de1978dadb7f2
Stephen Cases <builtforsomethingbigger@gmail.com>
2020-03-21 22:25:23 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -56,10 +56,10 @@ def naming_system
                  sub_category: {
                    label: "Species",
                    sub_category: nil }
-                   }
                   }
                 }
               }
             }
           }
+        }
 end
